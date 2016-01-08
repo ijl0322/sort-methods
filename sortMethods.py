06f6d62b5df8285f8af51d3535b11d0d458b4e31
Isabel Lee <ijl0322@gmail.com>
2016-01-08 13:41:19 +0800

    add selection sort

--- sortMethods.py
+++ sortMethods.py
@@ -8,5 +8,21 @@ def bubbleSort(lists):
                 lists[num + 1] = temp
     return lists
 
-print bubbleSort([5,3,2,1])
+#print bubbleSort([5,3,2,1])
+
+def selectionSort(lists):
+    sortedList = []
+    while len(lists) > 0:
+        smallest = None
+        for elt in lists:
+            if smallest == None:
+                smallest = elt
+            elif smallest > elt:
+                smallest = elt
+        sortedList.append(smallest)
+        lists.remove(smallest)
+    return sortedList
+    
+print selectionSort([5,2,1,3])
                 
+                         
